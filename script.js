dfb3013c61ac870a0fc5f467b3139be7e1ff42a8
Alexandr Vasin <alexandr.vasin1@yandex.ru>
2022-12-02 21:55:09 +0300

    создание числа, преобразование в объект, умножение чисел объекта

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-let a = 5;
+let num = 266219;
+
+// преобразуем num в строку
+let numString = num.toString();
+
+// разобьем строку на отдельный символ и переопределим значение переменной num
+num = numString.split("");
+let n = num.length;
+let count = 1;
+
+for (let i = 0; i < n; i++) {
+    count *= num[i];
+}
+
+console.log(count);
