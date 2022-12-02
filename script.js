b688c7b21129cd225ed633ba0f51053ed3e39389
Alexandr Vasin <alexandr.vasin1@yandex.ru>
2022-12-02 22:06:52 +0300

    возведение в степень и вывод двух первый цифр

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let num = 266219;
 
-// преобразуем num в строку
+// 2. реобразуем num в строку
 let numString = num.toString();
 
 // разобьем строку на отдельный символ и переопределим значение переменной num
@@ -8,8 +8,14 @@ num = numString.split("");
 let n = num.length;
 let count = 1;
 
+// создадим цикл для вычисления произведения всех чисел
 for (let i = 0; i < n; i++) {
     count *= num[i];
 }
 
 console.log(count);
+
+// 3. возводим полученное число в 3 степень
+count = count ** 3;
+console.log(count);
+
