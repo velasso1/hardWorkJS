14c2c48c34667774be211949d889f643340719ba
Alexandr Vasin <alexandr.vasin1@yandex.ru>
2022-12-12 09:59:52 +0300

    выполнение усложненного задания 5 урока

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 'use strict';
 
-const someFunc = function (arg) {
-    if (typeof arg !== 'string') {
-        return 'that not a string';
-    }
 
-    let string = arg.trim();
+let arr = ['4342', '3560', '921', '25438', '1367', '2842', '4029'];
 
-    if (string.length >= 30) {
-        return (string.slice(0, 30) + '...');
-    } else {
-        return (string);
+let arrLength = arr.length;
+for (let i = 0; i < arrLength; i++) {
+    if (arr[i].charAt(0) === '4') {
+        console.log(arr[i]);
+    } else if (arr[i].charAt(0) === '2') {
+        console.log(arr[i]);
     }
-};
+}
+
+let n = 100;
+for (let i = 2; i <= n; i++) {
+    for (let j = 2; j < i; j++) {
+        if ((i % j == 0)) {
+            break;
+        } else {
+            console.log(i);
+            break;
+        }
+    }
+}
