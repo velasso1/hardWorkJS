b6c2a8b4f1afadc11a1077ee303c21b6a0814a66
Alexandr Vasin <alexandr.vasin1@yandex.ru>
2022-12-25 16:18:55 +0300

    выполнение 7 задания

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 'use strict';
 
 const arr = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
-const today = new Date().getDay();
-
+let today = new Date().getDay();
 let weekBlock = document.createElement('div');
+today = 2;
+
 document.body.append(weekBlock);
 
-arr.forEach((item) => {
-    arr.splice();
+for (let i = 0; i < arr.length; i++) {
 
-    if (item === 'Воскресенье' || item === 'Суббота') {
-        weekBlock.append(item);
-    } else {
-        weekBlock.append(item);
+    switch (true) {
+
+        case i === today:
+            arr[i] = `<b> ${arr[i]} </b>`;
+            break;
+
+        case i === 0 || i === 6:
+            arr[i] = `<i> ${arr[i]} </i>`;
+            break;
     }
-});
+}
 
-
-
-
-
+weekBlock.innerHTML = arr.join('<br>');
