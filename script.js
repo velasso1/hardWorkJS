9367c48445c828f2e68b23d6e2e14391405f2ee6
Alexandr Vasin <alexandr.vasin1@yandex.ru>
2023-01-07 15:03:02 +0300

    исправление

--- script.js
+++ script.js
@@ -2,41 +2,63 @@
 
 const greet = document.createElement('p');
 const today = document.createElement('p');
-const time = document.createElement('p');
+const timing = document.createElement('p');
 const toNewYear = document.createElement('p');
 const days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
 
-let date = new Date();
-let hours = date.getHours();
-let minutes = date.getMinutes();
-let sec = date.getSeconds();
-let nextYear = new Date((date.getFullYear() + 1), 0, 1);
+const getDates = function () {
+    let date = new Date();
+    let day = date.getDay();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let nextYear = new Date(date.getFullYear() + 1, 0, 1);
 
-switch (true) {
-    case hours >= 23 || hours < 5:
-        greet.innerHTML = 'Доброй ночи!';
-        break;
+    return { date, day, hours, minutes, seconds, nextYear };
+};
 
-    case hours >= 18:
-        greet.innerHTML = 'Добрый вечер!';
-        break;
+const writing = function () {
+    let time = getDates();
 
-    case hours >= 9:
-        greet.innerHTML = 'Добрый день!';
-        break;
+    switch (true) {
+        case time.hours >= 23 || time.hours < 5:
+            greet.innerHTML = 'Доброй ночи!';
+            break;
 
-    case hours >= 5:
-        greet.innerHTML = 'Доброе утро!';
-        break;
-}
+        case time.hours >= 18:
+            greet.innerHTML = 'Добрый вечер!';
+            break;
 
-today.innerHTML = `Сегодня: ${days[date.getDay()]}`;
-time.innerHTML = `Текущее время: ${date.toLocaleTimeString('en')}`;
-toNewYear.innerHTML = `До нового года осталось ${Math.floor((nextYear - date) / 1000 / 24 / 3600)} дней`;
+        case time.hours >= 9:
+            greet.innerHTML = 'Добрый день!';
+            break;
+
+        case time.hours >= 5:
+            greet.innerHTML = 'Доброе утро!';
+            break;
+    }
+
+    time.hours = time.hours.toString().padStart(2, '0');
+    time.minutes = time.minutes.toString().padStart(2, '0');
+    time.seconds = time.seconds.toString().padStart(2, '0');
+
+    today.innerHTML = `Сегодня: ${days[time.day]}`;
+    timing.innerHTML = `Сейчас: ${time.hours + ':' + time.minutes + ':' + time.seconds}`;
+    toNewYear.innerHTML = `До нового года осталось ${Math.floor((time.nextYear - time.date) / 1000 / 24 / 3600)} дней`;
+
+    document.body.append(greet);
+    document.body.append(today);
+    document.body.append(timing);
+    document.body.append(toNewYear);
+};
+
+writing();
+
+setInterval(writing, 1000);
 
 
 
-document.body.append(greet);
-document.body.append(today);
-document.body.append(time);
-document.body.append(toNewYear);
+
+
+
+
